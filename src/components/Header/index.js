0c250d9ed2420bb02d1658b39f12fbf6d1c96ff8
Moisés Mariano <moisesduartem@gmail.com>
2020-08-01 19:05:22 -0300

    feat: componente Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+class Header extends Component {
+    render() {
+        return (<header id="main-header">
+            <h2><FontAwesomeIcon icon={faHome} /> QualMeuCEP?</h2>
+        </header>);
+    }
+}
+
+export default Header;
