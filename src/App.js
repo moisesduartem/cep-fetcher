a5fc754c9a09e91d568ae96c33bb940abe95bdb0
Moisés Mariano <moisesduartem@gmail.com>
2020-08-01 16:32:08 -0300

    testa font awesome icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <FontAwesomeIcon icon={faHome} />
       </div>
     );
   }
