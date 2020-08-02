1d97e2e8d521cf52aea24e405cd556380af95209
Moisés Mariano <moisesduartem@gmail.com>
2020-08-01 22:39:35 -0300

    fix: inclui icon (>) e insere texto filho

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../components/Button';
 import './styles.css'; 
 
 class Main extends Component {
+
+    state = {
+        gender: ['o', 'a', 'x'],
+    };
+
+    randomGenderViewer = () => {
+
+    }
+
+    showThisOption = () => (
+        alert('Aqui')
+    );
+
     render() {
+
+        const angleRight = <FontAwesomeIcon icon={faAngleRight} />
+
         return (<div id="main">
             <div className="title-box">
-                <h1>Bem-vindx!</h1>
+                {/* <h1>{`Bem-vind${this.state.gender}!`}</h1> */}
             </div>
 
             <h2>Buscar endereço por:</h2>
-            <Button label="CEP"/>
-            <Button label="Formulário" />
+            <Button option="1" icon={angleRight}>CEP</Button>
+            <Button option="2" icon={angleRight}>Outros dados</Button>
 
 
         </div>);
