baa71ed8a98d7d4d71010d32dcc155e309f2134a
Moisés Mariano <moisesduartem@gmail.com>
2020-08-03 11:30:44 -0300

    feat: input & button 'Voltar'

--- src/pages/byCep/index.js
+++ src/pages/byCep/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from '../../components/Button';
+import Feed from '../../components/Feed';
 import './styles.css'; 
 
 class byCep extends Component {
@@ -8,12 +9,12 @@ class byCep extends Component {
 
     render() {
 
-        return (<div id="main">
+        return (<Feed style={{ display: 'flex', flexDirection: 'row' }}>
             <form>
-                <input type="text" placeholder="Bla bla" />
+                <input placeholder="Digite o CEP..."></input>
             </form>
-            <Button>Voltar</Button>
-        </div>);
+            <Button style={{ display: 'flex', justifyContent: 'space-between' }} to="/">Voltar</Button>
+        </Feed>);
     }
 }
 
