729d5f062c5c55bbdfaea45512b8c335c1cf805d
Moisés Mariano <moisesduartem@gmail.com>
2020-08-03 18:02:27 -0300

    feat: onClick

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 class Button extends Component {
     render() {
         return (
-        <Link style={this.props.style} disabled={this.props.disabled}  to={this.props.to}>
+        <Link onClick={this.props.onClick} style={this.props.style} disabled={this.props.disabled}  to={this.props.to}>
             {this.props.children}
         </Link>);
     }
