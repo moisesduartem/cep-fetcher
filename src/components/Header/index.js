e9a86c219427ee3442b0d159755d8e232fb96192
Moisés Mariano <moisesduartem@gmail.com>
2020-08-03 16:04:06 -0300

    fix: QualMeuCep? => CEP Fetcher

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 class Header extends Component {
     render() {
         return (<header id="main-header">
-            <h2><FontAwesomeIcon icon={faHome} /> QualMeuCEP?</h2>
+            <h2><FontAwesomeIcon icon={faHome} /> CEP Fetcher</h2>
         </header>);
     }
 }
